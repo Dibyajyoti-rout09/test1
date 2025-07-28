774f1b3a20bcd7628e0b1fdb5048cddcff835f61
Dibya <dibyajyotirout2016@gmail.com>
2025-07-28 15:32:05 +0530

    Update TestApplication.java

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-         system.out.println("hiii");
+         system.out.println("hello how are u");
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
