9f585d82e6ff8db03f12bb880a36692421c61e05
Dibya <dibyajyotirout2016@gamil.com>
2025-07-28 11:59:16 +0530

    1st commit login feature

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
+         int x=10;
+         system.out.println(x);
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
